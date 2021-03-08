a8ab5082f9249e588df667d6633b01cf167ed62e
WataruTomita <tomi.symphonia@gmail.com>
2021-03-08 11:36:49 +0900

    rspec setting

---
+++ spec/ploplo_spec.rb
@@ -0,0 +1,12 @@
+describe "test" do
+  context 'tashizan' do
+    it '1 + 1は2になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context 'test' do
+    it '1 +1は2になる' do
+      expect(1 + 1).to eq 3
+    end
+  end
+end
