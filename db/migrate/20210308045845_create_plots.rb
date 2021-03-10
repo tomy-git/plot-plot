7245a88f833f0b324f995c79b10d5b8498a0a150
WataruTomita <tomi.symphonia@gmail.com>
2021-03-10 14:26:40 +0900

    [add ]migrate fix

--- db/migrate/20210308045845_create_plots.rb
+++ db/migrate/20210308045845_create_plots.rb
@@ -3,7 +3,8 @@ class CreatePlots < ActiveRecord::Migration[5.2]
     create_table :plots do |t|
       t.string :title
       t.string :body
-      t.integer :user_id
+      # t.integer :user_id
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210308050222_create_tagmaps.rb
+++ db/migrate/20210308050222_create_tagmaps.rb
@@ -2,8 +2,10 @@ class CreateTagmaps < ActiveRecord::Migration[5.2]
   def change
     create_table :tagmaps do |t|
 
-      t.integer :plot_id
-      t.integer :tag_id
+      # t.integer :plot_id
+      # t.integer :tag_id
+      t.references :plot, foreign_key: true
+      t.references :tag, foreign_key: true
       t.timestamps
     end
   end
