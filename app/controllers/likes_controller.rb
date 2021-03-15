0358950a866988f3ca6ff914c1e368ce3b047819
WataruTomita <tomi.symphonia@gmail.com>
2021-03-15 16:26:26 +0900

    [fix]like button del on

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,14 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(plot_id: params[:plot_id])
-    redirect_back(fallback_location: root_path)
+    @plot = Plot.find(params[:plot_id])
+    like = current_user.likes.create(plot_id: params[:plot_id])
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @like = Like.find_by(plot_id: params[:plot_id], user_id: current_user.id)
-    @like.destroy
-    redirect_back(fallback_location: root_path)
+    @plot = Plot.find(params[:plot_id])
+    like = Like.find_by(plot_id: params[:plot_id], user_id: current_user.id)
+    like.destroy
+    # redirect_back(fallback_location: root_path)
   end
 end
