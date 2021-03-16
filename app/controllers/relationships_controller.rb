08f41af00851171039c8b092dcf91cda45572c63
WataruTomita <tomi.symphonia@gmail.com>
2021-03-16 14:31:44 +0900

    [fix]Ajaxを使わなければ動く

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,16 +8,12 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    # current_user.follow(params[:id])
-    # render :create
     redirect_to user_path(@user)
   end
 
   def destroy
     @user = User.find(params[:id])
     current_user.unfollow(@user)
-    # current_user.unfollow(params[:id])
-    # render :destroy
     redirect_to user_path(@user)
   end
 end
