682639b9d3d11c6960ea3b05d9d1c1be75cc820d
WataruTomita <tomi.symphonia@gmail.com>
2021-03-23 14:08:28 +0900

    password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # パスワードリセット関連設定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Gmailでパスワードリセット送信
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "tomi.symphonia@gmail.com",
+    :password => "tjlzcwfqckjspaon",
+    :authentication => 'login'
+  }
 end
