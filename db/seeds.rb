99affe0ff9c96ffdf8836a33bec1a3ded167575a
WataruTomita <tomi.symphonia@gmail.com>
2021-03-25 18:30:34 +0900

    seeds rewrite

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# ユーザー生成
 User.create!(
   email:    ENV['EMAIL1'],
   name:     ENV['NAME1'],
@@ -24,8 +25,51 @@ User.create!(
   password: ENV['PASSWORD']
 )
 
+User.create!(
+  email:    'hoge2@hoge.com',
+  name:     'hoge2',
+  password: ENV['PASSWORD']
+)
+
+User.create!(
+  email:    'hoge3@hoge.com',
+  name:     'hoge3',
+  password: ENV['PASSWORD']
+)
+
+# プロット生成
 Plot.create!(
-  title: 'test1',
-  body: 'This is test plot.',
+  title: '桃太郎',
+  body: '<p>桃から生まれた</p>',
   user_id: 1,
+)
+
+Plot.create!(
+  title: 'かぐや姫',
+  body: '<p>竹から生まれた</p>',
+  user_id: 2,
+)
+
+Plot.create!(
+  title: '金太郎',
+  body: '<p>まさかり装備</p>',
+  user_id: 3,
+)
+
+Plot.create!(
+  title: '千夜一夜物語',
+  body: '<p>イスラームの世界観</p>',
+  user_id: 1,
+)
+
+Plot.create!(
+  title: 'シンデレラ',
+  body: '<p>原作では「灰被り」</p>',
+  user_id: 2,
+)
+
+Plot.create!(
+  title: '赤ずきん',
+  body: '<p>世界中に類話がある</p>',
+  user_id: 3,
 )
