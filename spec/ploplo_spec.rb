2596a774191273fb35690834cbc22b6d179f561e
WataruTomita <tomi.symphonia@gmail.com>
2021-04-21 16:37:27 +0900

    rspec test

--- spec/ploplo_spec.rb
+++ spec/ploplo_spec.rb
@@ -1,17 +1,16 @@
-describe "test" do
-  context 'tashizan' do
-    it '1 + 1は2になる' do
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Plot, 'modelテスト' do
+  # describe '保存機能' do
+  #   it '有効な投稿内容は保存されるか' do
+  #     expect(FactoryBot.build(:plot)).to be_valid
+  #   end
+  # end
+  describe "test" do
+    it '足し算' do
       expect(1 + 1).to eq 2
-    end
-  end
-  context 'test' do
-    it '1 + 1は2になる' do
-      expect(1 + 1).to eq 3
-    end
-  end
-  context "test" do
-    it '1 + 1は2になる' do
-      expect(1 + 1).to eq 4
     end
   end
 end
