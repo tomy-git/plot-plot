2023d5bfc21477f353b5f447a47de32778743d4a
WataruTomita <tomi.symphonia@gmail.com>
2021-05-02 09:35:47 +0900

    test code

--- spec/ploplo_spec.rb
+++ spec/ploplo_spec.rb
@@ -3,11 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Plot, 'modelテスト' do
-  # describe '保存機能' do
-  #   it '有効な投稿内容は保存されるか' do
-  #     expect(FactoryBot.build(:plot)).to be_valid
-  #   end
-  # end
   describe "test" do
     it '足し算' do
       expect(1 + 1).to eq 2
